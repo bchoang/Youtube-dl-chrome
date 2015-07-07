0558315d03f586fae026b1ef0f04cf405e90511c
Jackson Darrow <jackson.darrow@gmail.com>
2015-07-07 01:58:57 -0700

    added settings page, working mixtape copping build

--- popup.js
+++ popup.js
@@ -1,35 +1,59 @@
 document.addEventListener('DOMContentLoaded', function() {
     var link = document.getElementById('butt');
 
-    var xmlhttp = new XMLHttpRequest();
     // onClick's logic below:
     link.addEventListener('click', function(e) {
         e.preventDefault();
 
         chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
+
+            var info = {
+                destination: "",
+                quality: "",
+                inputTitle: "",
+                link: ""
+            };
+
             var url = tabs[0].url;
-            url = encodeURIComponent(url);
+            info.link = encodeURIComponent(url);
 
-            var filepath = document.getElementById("filepath").value;
-            filepath = encodeURIComponent(filepath);
-
-            var title = "";
             if (document.getElementById("filename").value != "*Leave blank for video title") {
-                var title = "=" + encodeURIComponent(document.getElementById("filename").value);
-            } 
-
-            xmlhttp.open("GET","https://localhost:6299/?destination=" + filepath + "&quality=1080p&filter&format=audio+only&title" + title + "&link=" + url,true);
-        
-            xmlhttp.onload = function() {
-                if (xmlhttp.readyState === 4) {
-                    console.log(xmlhttp.responseText);
-                } else {
-                    console.error(xmlhttp.statusText);
-                }
+                info.inputTitle = "=" + encodeURIComponent(document.getElementById("filename").value);
             }
 
-            xmlhttp.send();
+            chrome.storage.sync.get({
+                filepath: '',
+            }, function(items) {
+                console.log(items.filepath);
+                info.destination = encodeURIComponent(items.filepath);
 
+                downloadVideo(info);
+            });
         });
     });
 });
+
+function downloadVideo (info){
+    var xmlhttp = new XMLHttpRequest();
+
+    // turns info into a query string
+    var queryString = "";
+    for (var query in info){
+        if (info.hasOwnProperty(query) && info[query]){
+            queryString += query + '=' + info[query] + '&';
+        }
+    }
+    queryString = queryString.substring(0, queryString.length-1); // removes fencepost "&"
+    console.log(queryString);
+
+    xmlhttp.open("GET","https://localhost:6299/?" + queryString, true);
+
+    xmlhttp.onload = function() {
+        if (xmlhttp.readyState === 4) {
+            console.log(xmlhttp.responseText);
+        } else {
+            console.error(xmlhttp.statusText);
+        }
+    };
+    xmlhttp.send();
+}
