d2302247d5d56baf1ba020b8561ba84b4df41534
Jackson Darrow <jackson.darrow@gmail.com>
2015-07-07 22:53:21 -0700

    new look

--- popup.js
+++ popup.js
@@ -1,37 +1,82 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('butt');
 
-    // onClick's logic below:
-    link.addEventListener('click', function(e) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        if (url.indexOf('youtube') > 0 && url.indexOf('watch') > 0){
+            var toShow = document.getElementById('song');
+            removeClass(toShow, "hidden");
+            document.getElementById('songLink').value = url;
+
+        }
+        else if (url.indexOf('youtube.com') > 0 && url.indexOf('playlist') > 0){
+            var toShow = document.getElementById('playlist');
+            removeClass(toShow, "hidden");
+            document.getElementById('playlistLink').value = url;
+
+        }else{
+            var toShow = document.getElementById('song');
+            removeClass(toShow, "hidden");
+        }
+    });
+
+
+    document.getElementById('songButton').onclick = function(){
+        var toHide = document.getElementById('playlist');
+        addClass(toHide, "hidden");
+
+        var toShow = document.getElementById('song');
+        removeClass(toShow, "hidden");
+    };
+    document.getElementById('playlistButton').onclick = function(){
+        var toHide = document.getElementById('song');
+        addClass(toHide, "hidden");
+
+        var toShow = document.getElementById('playlist');
+        removeClass(toShow, "hidden");
+    };
+
+    document.getElementById('songSubmit').addEventListener('click', function(e) {
         e.preventDefault();
+        var info = {
+            destination: "",
+            quality: "",
+            inputTitle: "",
+            link: ""
+        };
 
-        chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
+        info.link = document.getElementById('songLink').value;
+        info.inputTitle = document.getElementById('songTitle').value;
 
-            var info = {
-                destination: "",
-                quality: "",
-                inputTitle: "",
-                link: ""
-            };
-
-            var url = tabs[0].url;
-            info.link = encodeURIComponent(url);
-
-            if (document.getElementById("filename").value != "*Leave blank for video title") {
-                info.inputTitle = "=" + encodeURIComponent(document.getElementById("filename").value);
-            }
-
-            chrome.storage.sync.get({
-                filepath: '',
-            }, function(items) {
-                console.log(items.filepath);
-                info.destination = encodeURIComponent(items.filepath);
-
-                downloadVideo(info);
-            });
+        chrome.storage.sync.get({
+            filepath: '',
+        }, function(items) {
+            info.destination = items.filepath;
+            downloadVideo(info);
         });
     });
+
+    document.getElementById('playlistSubmit').addEventListener('click', function(e) {
+        e.preventDefault();
+        var info = {
+            destination: "",
+            quality: "",
+            inputTitle: "",
+            link: ""
+        };
+
+        info.link = document.getElementById('songLink').value;
+        info.inputTitle = document.getElementById('songTitle').value;
+
+        chrome.storage.sync.get({
+            filepath: '',
+        }, function(items) {
+            info.destination = items.filepath;
+            downloadVideo(info);
+        });
+
+    });
 });
+
 
 function downloadVideo (info){
     var xmlhttp = new XMLHttpRequest();
@@ -40,7 +85,7 @@ function downloadVideo (info){
     var queryString = "";
     for (var query in info){
         if (info.hasOwnProperty(query) && info[query]){
-            queryString += query + '=' + info[query] + '&';
+            queryString += encodeURIComponent(query) + '=' + encodeURIComponent(info[query]) + '&';
         }
     }
     queryString = queryString.substring(0, queryString.length-1); // removes fencepost "&"
@@ -51,9 +96,22 @@ function downloadVideo (info){
     xmlhttp.onload = function() {
         if (xmlhttp.readyState === 4) {
             console.log(xmlhttp.responseText);
+            list.removeChild(li);
         } else {
             console.error(xmlhttp.statusText);
+            list.removeChild(li);
         }
     };
     xmlhttp.send();
+}
+
+function addClass (obj, newClass){
+    if (obj.className.indexOf(newClass) < 0){
+        obj.className = obj.className + " " + newClass;
+    }
+}
+
+function removeClass (obj, newClass){
+    if (obj.className.indexOf(newClass) > 0)
+        obj.className = obj.className.replace(" " + newClass, "");
 }
